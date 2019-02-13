73f742ae9306828ffd07ddd2352cb8f52a2c683e
bmdym <44413630+bmdym@users.noreply.github.com>
2019-02-12 20:32:55 -0700

    Added accessibility features
    Do they actually work? Who knows! Still testing. Also added 1 image. More to come.

--- phone.js
+++ phone.js
@@ -2,19 +2,36 @@ $(document).ready(function() {
   $("#content_dialer").show();
   $("#content_contacts").hide();
   $("#content_add").hide();
+  $("#content_help").hide();
 });
 $("#Dialer").click(function() {
   $("#content_dialer").show();
   $("#content_contacts").hide();
   $("#content_add").hide();
+  $("#content_help").hide();
 });
 $("#Contacts").click(function() {
   $("#content_contacts").show();
   $("#content_dialer").hide();
   $("#content_add").hide();
+  $("#content_help").hide();
 });
 $("#Add_Contact").click(function() {
   $("#content_add").show();
   $("#content_dialer").hide();
   $("#content_contacts").hide();
+  $("#content_help").hide();
 });
+$("#Help").click(function() {
+  $("#content_help").show();
+  $("#content_dialer").hide();
+  $("#content_contacts").hide();
+  $("#content_add").hide();
+});
+$("#content_dialer button").click(function() {
+	$("#input").val($("#input").val() + this.innerText);
+})
+
+$("#Clear").click(function() {
+	$("#input").val("");
+})
